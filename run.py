eb4eafede2265ee4660b70ebebe4def381fdf7e8
Barryhack <angwepony@gmail.com>
2025-01-13 20:46:13 +0100

    refactor: update run.py for production

--- run.py
+++ run.py
@@ -1,17 +1,8 @@
 """Application entry point."""
-import os
 from app import create_app
-from config import DevelopmentConfig
-import logging
+from config import ProductionConfig
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-def main():
-    app = create_app(DevelopmentConfig)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+app = create_app(ProductionConfig)
 
 if __name__ == '__main__':
-    main()
+    app.run()
